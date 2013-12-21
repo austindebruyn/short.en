79de7055ba23e28aad9389196451aafec19e4cc9
Austin deBruyn <austin.debruyn@yahoo.com>
2013-12-21 16:04:35 -0600

    fix triple equal signs

--- testing.php
+++ testing.php
@@ -12,40 +12,40 @@
 		function testClearDatabase() {
 			global $db;
 			$db->clearDatabase();
-			$this->assertTrue($db->countEntries() === 0);
+			$this->assertTrue($db->countEntries() == 0);
 		}
 
 		function testAddAlias1() {
 			global $db;
 			$result = $db->addAlias("test1", "http://1");
 			$this->assertTrue($result);
-			$this->assertTrue($db->countEntries() === 1);
+			$this->assertTrue($db->countEntries() == 1);
 		}
 
 		function testAddAlias2() {
 			global $db;
 			$result = $db->addAlias("test2", "http://2");
 			$this->assertTrue($result);
-			$this->assertTrue($db->countEntries() === 2);
+			$this->assertTrue($db->countEntries() == 2);
 		}
 
 		function testDuplicateAlias() {
 			global $db;
 			$result = $db->addAlias("test1", "http://3");
 			$this->assertFalse($result);
-			$this->assertTrue($db->countEntries() === 2);
+			$this->assertTrue($db->countEntries() == 2);
 		}
 		
 		function testFetchAlias() {
 			global $db;
 			$result = $db->fetchAlias("test1");
-			$this->assertTrue($result === "http://1");
+			$this->assertTrue($result == "http://1");
 		}
 		
 		function cleanupDatabase() {
 			global $db;
 			$db->clearDatabase();
-			$this->assertTrue($db->countEntries() === 0);
+			$this->assertTrue($db->countEntries() == 0);
 		}
 	}
 
