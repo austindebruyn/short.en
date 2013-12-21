ae6f68f3aa1d00410622bdf55b6624986be01aee
Austin deBruyn <austin.debruyn@yahoo.com>
2013-12-21 16:34:18 -0600

    security function unit tests

--- testing.php
+++ testing.php
@@ -1,4 +1,5 @@
 <?php
+	require_once('inc/functions.php');
 	require_once('inc/connect.php');
 	require_once('simpletest/autorun.php');
 
@@ -46,6 +47,39 @@
 			global $db;
 			$db->clearDatabase();
 			$this->assertTrue($db->countEntries() == 0);
+		}
+	}
+
+	class SecurityTests extends UnitTestCase {
+
+		function validURL_1() {
+			$url1 = "";
+			$url2 = "hello world";
+			$url3 = "><script type='text/javascript'></script><'";
+
+			$this->assertFalse(validURL($url1));
+			$this->assertFalse(validURL($url2));
+			$this->assertFalse(validURL($url3));
+		}
+
+		function validURL_2() {
+			$url1 = "http://google.com";
+			$url2 = "wikipedia.org/en/Computer";
+			$url3 = "ftp://short.en/";
+			$url4 = "https://testing.com/hy-phens.js";
+		
+			$this->assertTrue(validURL($url1));
+			$this->assertTrue(validURL($url2));
+			$this->assertTrue(validURL($url3));
+			$this->assertTrue(validURL($url4));
+		}
+
+		function testRedirectLoop() {
+			$url1 = "short.en/Test";
+			$url2 = "http://www.short.en/";
+
+			$this->assertFalse(validURL($url1));
+			$this->assertFalse(validURL($url2));	
 		}
 	}
 
