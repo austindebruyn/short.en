d67f2f6a49bc6cce4380c59a3bc08884e7c5b06b
Austin deBruyn <austin.debruyn@yahoo.com>
2013-12-21 15:12:25 -0600

    connection skeleton done, tests ready

--- inc/connect.php
+++ inc/connect.php
@@ -12,17 +12,54 @@
 		die($print);
 	}
 
-	//Open the dbconfig file to read host, user, pass
-	$fname = "inc/dbconfig"; 
-	if (!$handle = fopen($fname, 'r+'))
-		abort("Couldn't open dbconfig. Perhaps you haven't installed?");
-	$host = rtrim(fgets($handle), "\r\n");
-	$user = rtrim(fgets($handle), "\r\n");
-	$pass = rtrim(fgets($handle), "\r\n");
-	fclose($handle);
+	class databaseConnection {
 
-	$con = mysqli_connect($host, $user, $pass, 'shorten');
-	if (mysqli_connect_errno())
-		abort("MySQL Error: " . mysqli_connect_error());
+		private $numEntries = 0;
+		private $con;
+
+		public $connected = false;
+
+		function __construct() {
+			//Open the dbconfig file to read host, user, pass
+			$fname = "inc/dbconfig"; 
+			if (!$handle = fopen($fname, 'r+')) 
+				abort("Couldn't open dbconfig. Perhaps you haven't installed?");
+			else {
+				$host = rtrim(fgets($handle), "\r\n");
+				$user = rtrim(fgets($handle), "\r\n");
+				$pass = rtrim(fgets($handle), "\r\n");
+				fclose($handle);
+
+				$this->con = mysqli_connect($host, $user, $pass, 'shorten');
+				if (mysqli_connect_errno())
+					abort("MySQL Error: " . mysqli_connect_error());
+				else
+					$this->connected = true;
+			}
+		}
+
+		function __destruct() {
+			if ($this->connected)
+				$this->con->close();
+		}
+
+		public function countEntries() {
+
+		}
+
+		public function addAlias($alias, $href) {
+
+		}
+
+		public function fetchAlias($alias) {
+
+		}
+
+		public function clearDatabase() {
+
+		}
+	}
+
+	$db = new databaseConnection();
 
 ?>
--- testing.php
+++ testing.php
@@ -1,44 +1,52 @@
 <?php
-	require_once('simpletest/autorun.php');
 	require_once('inc/connect.php');
+	require_once('simpletest/autorun.php');
 
 	class DatabaseTest extends UnitTestCase {
 
 		function testConnection() {
+			global $db;
 			$this->assertTrue($db->connected);
 		}
 
 		function testClearDatabase() {
+			global $db;
 			$db->clearDatabase();
 			$this->assertTrue($db->countEntries() === 0);
 		}
 
 		function testAddAlias1() {
+			global $db;
 			$result = $db->addAlias("test1", "http://1");
 			$this->assertTrue($result);
 			$this->assertTrue($db->countEntries() === 1);
 		}
 
 		function testAddAlias2() {
+			global $db;
 			$result = $db->addAlias("test2", "http://2");
 			$this->assertTrue($result);
 			$this->assertTrue($db->countEntries() === 2);
 		}
 
 		function testDuplicateAlias() {
+			global $db;
 			$result = $db->addAlias("test1", "http://3");
 			$this->assertFalse($result);
 			$this->assertTrue($db->countEntries() === 2);
 		}
 		
 		function testFetchAlias() {
+			global $db;
 			$result = $db->fetchAlias("test1");
 			$this->assertTrue($result === "http://1");
 		}
 		
 		function cleanupDatabase() {
+			global $db;
 			$db->clearDatabase();
 			$this->assertTrue($db->countEntries() === 0);
 		}
 	}
+
 ?>
